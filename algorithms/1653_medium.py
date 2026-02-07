cb0949c6bcd812c774c2002e8177538417587e48
sksy <rsposkrebyshev@gmail.com>
2026-02-07 23:45:30 +0300

    Added solution for task 1653 on lc

---
+++ algorithms/1653_medium.py
@@ -0,0 +1,13 @@
+# You are given a string s consisting only of characters 'a' and 'b'.
+# You can delete any number of characters in s to make s balanced.
+# s is balanced if there is no pair of indices (i,j) such that i < j and s[i] = 'b' and s[j]= 'a'.
+# Return the minimum number of deletions needed to make s balanced.
+
+class Solution:
+    def minimumDeletions(self, s: str) -> int:
+        cnt = 0
+        while s.count('ba') != 0:
+                print(s, s.count('ba'))
+                cnt += s.count('ba')
+                s = s.replace('ba', '')
+        return cnt
